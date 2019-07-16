3176886aed479217696959afa290fb9af3edafa7
muresanciprian99 <muresan.ciprian99@gmail.com>
2019-07-16 15:33:21 +0300

    created function that reads image;created .gitignore config to ignore image files used for test purposes

--- image_load/image_load.php
+++ image_load/image_load.php
@@ -1,1 +1,23 @@
 <?php
+
+
+function imageLoad(array $inputInfo) : array
+{
+    if (!isset($inputInfo["input-file"]))
+        die;
+    $image = new Imagick();
+    if (!$image->readImage($inputInfo["input-file"]))
+        die;
+    $inputInfo["image"] = $image;
+    return $inputInfo;
+}
+
+
+function imageLoadTests()
+{
+    $inputInfo["input-file"] = "/home/ciprianmuresan/PhpstormProjects/Image_Edit_Utility_CLI/pexels-photo-414612.jpeg";
+    $inputWithImage = imageLoad($inputInfo);
+    assert(isset($inputWithImage["image"]) === TRUE);
+}
+
+imageLoadTests();
