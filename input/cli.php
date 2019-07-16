8d17c20099719bd18d13fdd6f8b71a9c72327611
muresanciprian99 <muresan.ciprian99@gmail.com>
2019-07-16 15:09:27 +0300

    rename CLI.php to cli.php;created test for parsing input array

---
+++ input/cli.php
@@ -0,0 +1,46 @@
+<?php
+use PHPImageWorkshop\ImageWorkshop;
+
+function parseCommandLineArguments(array $args) : array
+{
+    $info = [];
+    foreach ($args as $option) {
+        @list($key, $value) = explode("=", $option);
+        $key = ltrim($key, "-");
+        $info[$key] = $value;
+    }
+    return $info;
+}
+
+function testParseCommandLineArguments()
+{
+    $argsTest = [
+        "--input-file=path",
+        "--output-file=path",
+        "--width=30",
+        "--height=40"
+    ];
+    $argsTestResult = parseCommandLineArguments($argsTest);
+    assert($argsTestResult === [
+            "input-file" => "path",
+            "output-file" => "path",
+            "width" => "30",
+            "height" => "40"]);
+
+    $argsTest = [
+        "--input-files=path",
+        "--output-files=",
+        "--width:30",
+        "--height=40px"
+    ];
+    $argsTestResult = parseCommandLineArguments($argsTest);
+    assert($argsTestResult === [
+            "input-files" => "path",
+            "output-files" => "",
+            "width:30" => NULL,
+            "height" => "40px"
+        ]);
+}
+
+testParseCommandLineArguments();
+//parseCommandLineArguments($argv);
