e7d1c2aba209e4874ab14680b7f67c66ce74d198
muresanciprian99 <muresan.ciprian99@gmail.com>
2019-07-17 16:55:28 +0300

    added function to display if the image was successfully edited and saved.

--- index.php
+++ index.php
@@ -5,83 +5,85 @@ include_once "image_operations/width.php";
 include_once "image_operations/height.php";
 include_once "watermark/watermark.php";
 include_once "image_operations/ratio.php";
+include_once "image_save/image_save.php";
+include_once "output/showSuccess.php";
 
-$arrayTest = [
-    'input-file' => 'pexels-photo-414612.jpeg',
-    'output-file' => 'itWorks.jpg',
-    'width' => '1920'
-];
-//$info = parseCommandLineArguments($argv);
-$info = imageLoad($arrayTest);
-$img = $info["image"]->getImageBlob();
-$img = base64_encode($img);
-var_dump("Original.");
-echo("<img src='data:image/jpg;base64,");
-echo($img);
-echo("'/><br\/><br\/>");
-$info = width($info);
-$img = $info["image"]->getImageBlob();
-$img = base64_encode($img);
-var_dump("Width changed.");
-echo("<img src='data:image/jpg;base64,");
-echo($img);
-echo("'/><br\/><br\/>");
-$info["height"] = "1080";
-$info = height($info);
-$img = $info["image"]->getImageBlob();
-$img = base64_encode($img);
-var_dump("Height changed.");
-echo("<img src='data:image/jpg;base64,");
-echo($img);
-echo("'/><br\/><br\/>");
-$info["watermark"] = "/home/ciprianmuresan/PhpstormProjects/Image_Edit_Utility_CLI/C1Iuu2FH_400x400.jpg";
-$info = watermark($info);
-$img = $info["image"]->getImageBlob();
-$img = base64_encode($img);
-var_dump("Watermark added.");
-echo("<img src='data:image/jpg;base64,");
-echo($img);
-echo("'/><br\/><br\/>");
-
-
-
-$arrayTest = [
-    'input-file' => 'pexels-photo-414612.jpeg',
-    'output-file' => 'itWorks.jpg',
-    'width' => '1440',
-    'format' => '16:9'
-];
-//$info = parseCommandLineArguments($argv);
-$info = imageLoad($arrayTest);
-$info = ratio($info);
-$img = $info["image"]->getImageBlob();
-$img = base64_encode($img);
-var_dump("Aspect ratio with width.");
-echo("<img src='data:image/jpg;base64,");
-echo($img);
-echo("'/><br\/><br\/>");
-
-$info["format"] = "4:1";
-unset($info["width"]);
-$info["height"] = "300";
-$info = ratio($info);
-$img = $info["image"]->getImageBlob();
-$img = base64_encode($img);
-var_dump("Aspect ratio with height.");
-echo("<img src='data:image/jpg;base64,");
-echo($img);
-echo("'/><br\/><br\/>");
-
-$arrayTest = [
-    'input-file' => 'pexels-photo-414612.jpeg',
-    'output-file' => 'itWorks.jpg',
-    'format' => '16:9'
-];
-$info = imageLoad($arrayTest);
-$info = ratio($info);
-$img = $info["image"]->getImageBlob();
-$img = base64_encode($img);
-var_dump("Aspect ratio only.");
-echo("<img src='data:image/jpg;base64,");
-echo($img);
-echo("'/><br\/><br\/>");
+$info = parseCommandLineArguments($argv);
+//$arrayTest = [
+//    'input-file' => 'pexels-photo-414612.jpeg',
+//    'output-file' => 'itWorks.jpg',
+//    'width' => '1920'
+//];
+//$info = imageLoad($arrayTest);
+//$img = $info["image"]->getImageBlob();
+//$img = base64_encode($img);
+//var_dump("Original.");
+//echo("<img src='data:image/jpg;base64,");
+//echo($img);
+//echo("'/><br\/><br\/>");
+//$info = width($info);
+//$img = $info["image"]->getImageBlob();
+//$img = base64_encode($img);
+//var_dump("Width changed.");
+//echo("<img src='data:image/jpg;base64,");
+//echo($img);
+//echo("'/><br\/><br\/>");
+//$info["height"] = "1080";
+//$info = height($info);
+//$img = $info["image"]->getImageBlob();
+//$img = base64_encode($img);
+//var_dump("Height changed.");
+//echo("<img src='data:image/jpg;base64,");
+//echo($img);
+//echo("'/><br\/><br\/>");
+//$info["watermark"] = "/home/ciprianmuresan/PhpstormProjects/Image_Edit_Utility_CLI/C1Iuu2FH_400x400.jpg";
+//$info = watermark($info);
+//$img = $info["image"]->getImageBlob();
+//$img = base64_encode($img);
+//var_dump("Watermark added.");
+//echo("<img src='data:image/jpg;base64,");
+//echo($img);
+//echo("'/><br\/><br\/>");
+//
+//
+//
+//$arrayTest = [
+//    'input-file' => 'pexels-photo-414612.jpeg',
+//    'output-file' => 'itWorks.jpg',
+//    'width' => '1440',
+//    'format' => '16:9'
+//];
+////$info = parseCommandLineArguments($argv);
+//$info = imageLoad($arrayTest);
+//$info = ratio($info);
+//$img = $info["image"]->getImageBlob();
+//$img = base64_encode($img);
+//var_dump("Aspect ratio with width.");
+//echo("<img src='data:image/jpg;base64,");
+//echo($img);
+//echo("'/><br\/><br\/>");
+//
+//$info["format"] = "4:1";
+//unset($info["width"]);
+//$info["height"] = "300";
+//$info = ratio($info);
+//$img = $info["image"]->getImageBlob();
+//$img = base64_encode($img);
+//var_dump("Aspect ratio with height.");
+//echo("<img src='data:image/jpg;base64,");
+//echo($img);
+//echo("'/><br\/><br\/>");
+//
+//$arrayTest = [
+//    'input-file' => 'pexels-photo-414612.jpeg',
+//    'output-file' => 'itWorks.jpg',
+//    'format' => '16:9'
+//];
+//$info = imageLoad($arrayTest);
+//$info = ratio($info);
+//$img = $info["image"]->getImageBlob();
+//$img = base64_encode($img);
+//var_dump("Aspect ratio only.");
+//echo("<img src='data:image/jpg;base64,");
+//echo($img);
+//echo("'/><br\/><br\/>");
---
+++ output/showSuccess.php
@@ -0,0 +1,9 @@
+<?php
+
+function showSuccess(array $inputInfo) : void
+{
+    if (!isset($inputInfo["success"]) && $inputInfo["success"] === TRUE)
+        echo "Image saved successfully at ".$inputInfo["output-file"].PHP_EOL;
+    else
+        echo "Image couldn't be saved.".PHP_EOL;
+}
