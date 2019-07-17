9079b673d863778fdd66a089533fd297722038b2
muresanciprian99 <muresan.ciprian99@gmail.com>
2019-07-17 10:45:38 +0300

    added watermark;displayed an example in index.php;now what to do with ratio?

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 include_once "input/cli.php";
 include_once "image_load/image_load.php";
 include_once "image_operations/width.php";
-include "image_operations/height.php";
+include_once "image_operations/height.php";
+include_once "watermark/watermark.php";
 
 $arrayTest = [
     'input-file' => 'pexels-photo-414612.jpeg',
@@ -32,6 +33,17 @@ echo($img);
 echo("'/><br\/><br\/>");
 $info["height"] = "1080";
 $info = height($info);
+ob_start();
+$img = $info["image"]->getImageBlob();
+$content = ob_get_contents();
+ob_end_clean();
+$img = base64_encode($img);
+var_dump($img);
+echo("<img src='data:image/jpg;base64,");
+echo($img);
+echo("'/><br\/><br\/>");
+$info["watermark"] = "/home/ciprianmuresan/PhpstormProjects/Image_Edit_Utility_CLI/C1Iuu2FH_400x400.jpg";
+$info = watermark($info);
 ob_start();
 $img = $info["image"]->getImageBlob();
 $content = ob_get_contents();
---
+++ watermark/watermark.php
@@ -0,0 +1,18 @@
+<?php
+
+function watermark(array $inputInfo) : array
+{
+    // this is canExecute();
+    if (!isset($inputInfo["watermark"]) || $inputInfo["watermark"] == NULL) //check if we have a watermark value
+        die;
+    if (!isset($inputInfo["image"])) // check if we have an image
+        die;
+    $newImage = $inputInfo["image"]->getImage();
+    $watermark = new Imagick();
+    $watermark->readImage($inputInfo["watermark"]);
+    $watermark->setImageOpacity(0.1);
+    //this is execute()
+    $newImage->compositeImage($watermark, Imagick::COMPOSITE_OVER, 0, 0);
+    $inputInfo["image"] = $newImage;
+    return $inputInfo;
+}
