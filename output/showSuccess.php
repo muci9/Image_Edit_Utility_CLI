99b002148774bd1916182f2a7bfed8fcff759575
muresanciprian99 <muresan.ciprian99@gmail.com>
2019-07-17 17:10:36 +0300

    showSuccess had a bad if.

--- output/showSuccess.php
+++ output/showSuccess.php
@@ -2,7 +2,7 @@
 
 function showSuccess(array $inputInfo) : void
 {
-    if (!isset($inputInfo["success"]) && $inputInfo["success"] === TRUE)
+    if (isset($inputInfo["success"]) || $inputInfo["success"] === TRUE)
         echo "Image saved successfully at ".$inputInfo["output-file"].PHP_EOL;
     else
         echo "Image couldn't be saved.".PHP_EOL;
