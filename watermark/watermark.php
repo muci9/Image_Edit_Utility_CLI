66b148be988f882cf870d32244ec467c0172b355
muresanciprian99 <muresan.ciprian99@gmail.com>
2019-07-17 14:51:10 +0300

    the corner of the watermark is put in a random corner now

--- watermark/watermark.php
+++ watermark/watermark.php
@@ -1,5 +1,27 @@
 <?php
 
+function getRandomCorner(Imagick $targetImage, Imagick $watermark) : array
+{
+    $maxWidth = $targetImage->getImageWidth() - $watermark->getImageWidth();
+    $maxHeight = $targetImage->getImageHeight() - $watermark->getImageHeight();
+    $arrCorner = [
+        [0, 0],
+        [0, $maxHeight],
+        [$maxWidth, 0],
+        [$maxWidth, $maxHeight]
+    ];
+    return $arrCorner[array_rand($arrCorner)];
+}
+
+function getWatermark(string $watermarkPath, Imagick $targetImage) : Imagick
+{
+    $watermark = new Imagick();
+    $watermark->readImage($watermarkPath);
+    $watermark->setImageOpacity(0.25);
+    $watermark->scaleImage($targetImage->getImageWidth()/ 10, $targetImage->getImageHeight() / 10, TRUE);
+    return $watermark;
+}
+
 function watermark(array $inputInfo) : array
 {
     // this is canExecute();
@@ -8,12 +30,12 @@ function watermark(array $inputInfo) : array
     if (!isset($inputInfo["image"])) // check if we have an image
         die;
     $newImage = $inputInfo["image"]->getImage();
-    $watermark = new Imagick();
-    $watermark->readImage($inputInfo["watermark"]);
-    $watermark->setImageOpacity(0.1);
-    $watermark->scaleImage($newImage->getImageWidth()/ 10, $newImage->getImageHeight() / 10, TRUE);
+    //prepare watermark, possible to move to image_load
+    $watermark = getWatermark($inputInfo["watermark"], $newImage);
+    //get position for watermark
+    list($x, $y) = getRandomCorner($newImage, $watermark);
     //this is execute()
-    $newImage->compositeImage($watermark, Imagick::COMPOSITE_OVER, 0, 0);
+    $newImage->compositeImage($watermark, Imagick::COMPOSITE_OVER, $x, $y);
     $inputInfo["image"] = $newImage;
     return $inputInfo;
 }
