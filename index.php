581b65ef48a0190975279244c93aaef4886ddf75
muresanciprian99 <muresan.ciprian99@gmail.com>
2019-07-17 10:23:47 +0300

    forgot to includ height.php

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 include_once "input/cli.php";
 include_once "image_load/image_load.php";
 include_once "image_operations/width.php";
+include "image_operations/height.php";
 
 $arrayTest = [
     'input-file' => 'pexels-photo-414612.jpeg',
     'output-file' => 'itWorks.jpg',
-    'width' => '1000'
+    'width' => '1920'
 ];
 //$info = parseCommandLineArguments($argv);
 $info = imageLoad($arrayTest);
@@ -29,7 +30,7 @@ var_dump($img);
 echo("<img src='data:image/jpg;base64,");
 echo($img);
 echo("'/><br\/><br\/>");
-$info["height"] = "2000";
+$info["height"] = "1080";
 $info = height($info);
 ob_start();
 $img = $info["image"]->getImageBlob();
