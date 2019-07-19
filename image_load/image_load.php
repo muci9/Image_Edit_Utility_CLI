329088fc926ad83c42cd7ea0d14461f24aae8214
muresanciprian99 <muresan.ciprian99@gmail.com>
2019-07-19 10:50:38 +0300

    modified .gitignore;added imageLoadController;

--- image_load/image_load.php
+++ image_load/image_load.php
@@ -4,25 +4,33 @@
 function imageLoadController(array $payload)
 {
     $payload = loadImage($payload);
-}
-function loadImage(array $inputInfo) : array
-{
-    if (!isset($inputInfo["input-file"]))
-        die;
-    $image = new Imagick();
-    if (!$image->readImage($inputInfo["input-file"]))
-        die;
-    $inputInfo["image"] = $image;
-    return $inputInfo;
+    if (isset($payload[ERRORS]))
+        errorsController($payload);
+    operationsController($payload);
 }
 
+function loadImage(array $payloadWithoutImage) : array
+{
+    $payloadWithImage = $payloadWithoutImage;
+    if (!isset($payloadWithImage["input-file"])) {
+        $payloadWithImage[ERRORS][] = "Image couldn't be loaded.";
+        return $payloadWithImage;
+    }
+    $image = new Imagick();
+    if (!$image->readImage($payloadWithImage["input-file"]))  {
+        $payloadWithImage[ERRORS][] = "Image couldn't be loaded.";
+        return $payloadWithImage;
+    }
+    $payloadWithImage["image"] = $image;
+    return $payloadWithImage;
+}
 
 function imageLoadTests()
 {
-    $inputInfo["input-file"] = "/home/ciprianmuresan/PhpstormProjects/Image_Edit_Utility_CLI/pexels-photo-414612.jpeg";
-    $inputWithImage = imageLoad($inputInfo);
+    $payloadWithImage["input-file"] = "/home/ciprianmuresan/PhpstormProjects/Image_Edit_Utility_CLI/pexels-photo-414612.jpeg";
+    $inputWithImage = loadImage($payloadWithImage);
     assert(isset($inputWithImage["image"]) === TRUE);
-    assert(get_class($inputInfo["image"] === "Imagick"));
+    assert(get_class($payloadWithImage["image"] === "Imagick"));
 }
 
-imageLoadTests();
+//imageLoadTests();
