88c4caf47192dd79f9a1cb5bae75962ec7875b46
muresanciprian99 <muresan.ciprian99@gmail.com>
2019-07-19 10:59:51 +0300

    added a new constant for the output message;added an errors controller;

--- constants.php
+++ constants.php
@@ -12,3 +12,4 @@ const ERRORS = "errors";
 const STANDARD_KEYS = [INPUT_FILE, OUTPUT_FILE, WIDTH, HEIGHT, FORMAT, WATERMARK, HELP, IMAGE];
 const INPUT_DIR = "/home/ciprianmuresan/PhpstormProjects/Image_Edit_Utility_CLI/input_dir";
 const OUTPUT_DIR = "/home/ciprianmuresan/PhpstormProjects/Image_Edit_Utility_CLI/output_dir";
+const OUTPUT_MESSAGE = "output-msg";
---
+++ errors/errors.php
@@ -0,0 +1,9 @@
+<?php
+
+function errorsController(array $payload)
+{
+    if (!isset($payload[ERRORS]))
+        $payload[ERRORS][] = "Something went terribly wrong. You shouldn't have been here.";
+    $payload[OUTPUT_MESSAGE] = implode(PHP_EOL, $payload[ERRORS]);
+    outputController($payload);
+}
